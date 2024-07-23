7f847292c007b25c2cb6d6e00f26242ad85d96c0
impepditerferendi <173524552+impepditerferendi@users.noreply.github.com>
2024-07-23 23:14:30 +0800

    did q8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
 const add = function(num1, num2) {
-	
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  return numArray.reduce((accumulator, currentValue)=>
+    accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((accumulator, currentValue)=>
+    accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let sum = 1;
+  if(n === 0){
+    return 1;
+  }
+
+  for(let i = n; i > 0; i--){
+    sum *= i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
