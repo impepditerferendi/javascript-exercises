d27653739ff96831f21580750dbcb941d6e854ed
impepditerferendi <173524552+impepditerferendi@users.noreply.github.com>
2024-07-31 05:31:33 +0800

    did 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^a-z0-9]+/gi, "");
+    word = word.toUpperCase();
+    let high = word.length-1;
+    let low = 0;
+    for(let i = 0; i < word.length; i++){
+    if(low >= high){
+        return true;
+    }else if(word.charAt(low) != word.charAt(high)){
+        return false;
+    }else{
+        low++;
+        high--;
+    }
+}
 };
+
+
+//npm test palindromes.spec.js
 
 // Do not edit below this line
 module.exports = palindromes;
